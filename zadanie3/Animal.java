653b72fb9d23972df4dccaaade200edb1b8a37b8
KozminaEkaterina <104621531+KozminaEkaterina@users.noreply.github.com>
2022-11-15 14:42:45 +0200

    Create Animal.java

---
+++ zadanie3/Animal.java
@@ -0,0 +1,13 @@
+package zadanie3;
+
+public class Animal {
+    protected  String name;
+
+    public Animal (String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return "Бобик";
+    }
+}
